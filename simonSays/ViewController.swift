6b781b66f45785a3e7b776fc14c8a8d4d675c04e
ALEJANDRO MORENO <rl106957@rlas-macair-36.rlas-116.org>
2018-09-19 12:18:30 -0500

    Initial Commit

---
+++ simonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simonSays
+//
+//  Created by ALEJANDRO MORENO on 9/19/18.
+//  Copyright © 2018 ALEJANDRO MORENO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
